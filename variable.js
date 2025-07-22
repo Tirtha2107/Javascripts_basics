432435d4f718170fa27766c884c26abcdc1744ff
Tirtha2107 <tirthakadam63@gmail.com>
2025-07-22 09:09:19 +0530

    first commit

---
+++ variable.js
@@ -0,0 +1,55 @@
+// var num=10; //declare
+// num=30; //assigned
+// console.log("before block scope",num);
+// {
+//     var num=20;
+//     console.log("inside block scope",num);
+// }
+// console.log("after block scope",num);
+
+
+
+// let num=10; //declare
+// num=20;
+// console.log("before block scope",num);
+// {
+//     let num=20;
+//     console.log("inside block scope",num);
+// }
+// console.log("after block scope",num);
+
+// const num=10; //declare
+// console.log("before block scope",num);
+// {
+//     const num=20;
+//     console.log("inside block scope",num);
+// }
+// console.log("after block scope",num);
+// // console.log(typeof num)
+
+// console.log(Number.MAX_SAFE_INTEGER);
+// let no= 999999999999999999n;
+// let no = BigInt("777777777777777777777777777")
+// console.log(no);
+// console.log(typeof no);
+
+// const user ={
+//     userName :"Arohi",
+//     age:18,
+//     isLoggedIn: true
+// }
+
+// // console.log(user);
+// console.log(user.age);
+// console.log(user["userName"]);
+
+let acTemp = null;
+
+console.log("hello world");
+
+
+
+console.log(acTemp);
+
+console.log(typeof"123");
+console.log(typeof true)
