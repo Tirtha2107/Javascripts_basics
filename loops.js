9b9e4a9526535de05316e9c3204cd085504861d4
Tirtha2107 <tirthakadam63@gmail.com>
2025-07-24 09:04:47 +0530

    day3

--- loops.js
+++ loops.js
@@ -1,11 +1,55 @@
-// for loop
+// // for loop
 
-for (let i=2;i<=10;i+=2)
-{
-    console.log(i);
+// for (let i=2;i<=10;i+=2)
+// {
+//     console.log(i);
     
+// }
+// for (let i = 5; i >= 1; i--) {
+//   console.log(i);
+// }
+
+// // for loop
+
+// for (let i = 1; i <= 10; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
+
+
+let sum = 0;
+for (let i = 1; i <= 4; i++) {
+  sum += i;
 }
-for (let i = 5; i >= 1; i--) {
+console.log(sum); //output=10
+
+for (let i = 1; i <= 10; i++) {
+  if (i === 5) {
+    break;
+  }
   console.log(i);
 }
 
+for (let i = 1; i <= 5; i++) {
+  if (i === 3) {
+    continue;
+  }
+  console.log(i);
+}
+
+// // while loop
+
+// let count = 3;
+// while (count > 0); {
+//   console.log(count);
+//   count--;
+// }
+
+// do..while loop
+
+let x = 10;
+do {
+  console.log("Value of x:", x);
+  x++;
+} while (x < 5);
---
+++ string.js
@@ -0,0 +1,29 @@
+let userName="Tirtha"
+let user = new String("Sai")
+console.log(typeof userName);
+console.log(typeof user);
+
+
+let userr="  sai   ";
+console.log(userr.length); //properties //length start with 1
+console.log(userr.toLowerCase());
+console.log(userr.toUpperCase());
+console.log(userr.charAt(2));
+console.log(userr.indexOf('a'));
+console.log(userr.indexOf('sai'));
+console.log(userr.indexOf('y')); //if not present in string
+
+let namne="  tirtha kadam  ";
+console.log(namne.trim());
+console.log(namne.replace('h','t'));
+console.log(namne.includes('ah'));
+console.log(namne.includes('ha'));
+
+let uName = "tirtha%priya%sai%yash%karan"
+console.log(uName.split('%'));
+
+let uuser ="saideep"
+console.log(uuser.substring(-4,5)); //(0,5)
+console.log(uuser.slice(-4,5));
+
+
